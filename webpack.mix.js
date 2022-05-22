aa38e1d02fcddc876d49b64b3d9075a15ebaabc7
Pierre-Philippe Emond <emondpph@gmail.com>
2022-05-22 00:48:23 -0400

    mix path

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,11 @@
 const mix = require('laravel-mix');
+const path = require('path');
 require('laravel-mix-polyfill');
 
 mix.js('vue/index.js', 'public/js/app.js');
 mix.vue({ version: 3, globalStyles: 'vue/app.scss', extractStyles: 'public/css/app.css', });
 mix.extract(['vue', 'vuex', 'quidphp-navigation','quidphp-browser','quidphp-javascript'], 'public/js/app-vendor.js');
+mix.alias({ '*': path.join(__dirname, 'react') });
 
 if(mix.inProduction())
 mix.polyfill({ enabled: true, useBuiltIns: false });
